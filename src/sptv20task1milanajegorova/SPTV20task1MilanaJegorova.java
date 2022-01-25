85e2be15ac669f0dba8d2db24d3666997e7c3534
Milana Jegorova <Milana.Jegorova@ivkhk.ee>
2022-01-25 19:33:10 +0200

    Task is done

---
+++ src/sptv20task1milanajegorova/SPTV20task1MilanaJegorova.java
@@ -0,0 +1,10 @@
+package sptv20task1milanajegorova;
+
+
+public class SPTV20task1MilanaJegorova {
+
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
